9ef9607de683566308ff4722fc77474748f6e451
Restyled.io <commits@restyled.io>
2020-07-11 20:44:11 +0000

    Restyled by reorder-python-imports

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python3
-
 # An espeak TTS binding for Python3.
 # Copyright (C) 2016 Sayak Brahmachari.
 ##
@@ -15,7 +14,6 @@
 ##
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import time
 
 import espeakng
