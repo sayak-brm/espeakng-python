0a24566c66b3db3f8dc0c47eb0817d089756bc1c
Sayak B <sayak-brm@users.noreply.github.com>
2020-07-12 02:27:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,46 +14,52 @@
 ##
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import time
 
 import espeakng
 
-print("Testing espeak4py\n")
+def test_espeakng():
+    print("Testing espeak4py\n")
 
-mySpeaker = espeakng.Speaker()
+    mySpeaker = espeakng.Speaker()
 
-mySpeaker.say("", wait4prev=True)
+    mySpeaker.say("", wait4prev=True)
 
-print("Testing wait4prev")
+    print("Testing wait4prev")
 
-mySpeaker.say("Hello, World!")
-time.sleep(1)
-mySpeaker.say("Interrupted!")
-time.sleep(3)
+    mySpeaker.say("Hello, World!")
+    time.sleep(1)
+    mySpeaker.say("Interrupted!")
+    time.sleep(3)
 
-mySpeaker.say("Hello, World!")
-time.sleep(1)
-mySpeaker.say("Not Interrupted.", wait4prev=True)
-time.sleep(3)
+    mySpeaker.say("Hello, World!")
+    time.sleep(1)
+    mySpeaker.say("Not Interrupted.", wait4prev=True)
+    time.sleep(3)
 
-print("Testing pitch")
+    print("Testing pitch")
 
-myHighPitchedSpeaker = espeakng.Speaker(pitch=99)
-myHighPitchedSpeaker.say("I am a demo of the say function")
-time.sleep(4)
+    myHighPitchedSpeaker = espeakng.Speaker(pitch=99)
+    myHighPitchedSpeaker.say("I am a demo of the say function")
+    time.sleep(4)
 
-print("Testing wpm")
+    print("Testing wpm")
 
-myFastSpeaker = espeakng.Speaker(wpm=140)
-myFastSpeaker.say("I am a demo of the say function")
+    myFastSpeaker = espeakng.Speaker(wpm=140)
+    myFastSpeaker.say("I am a demo of the say function")
 
-print("Testing parameter overrides with say")
-myFastSpeaker.say("I am a demo of the say function", wait4prev=True, wpm=240)
-time.sleep(4)
+    print("Testing parameter overrides with say")
+    myFastSpeaker.say("I am a demo of the say function", wait4prev=True, wpm=240)
+    time.sleep(4)
 
-print("Testing voice")
+    print("Testing voice")
 
-mySpanishSpeaker = espeakng.Speaker(voice="es")
-mySpanishSpeaker.say("Hola. Como estas?")
+    mySpanishSpeaker = espeakng.Speaker(voice="es")
+    mySpanishSpeaker.say("Hola. Como estas?")
 
-print("Testing Completed.")
+    print("Testing Completed.")
+
+if __name__ == "__main__":
+    test_espeakng()
+    
