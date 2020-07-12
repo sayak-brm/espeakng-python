18258ce874067f5bc6c20269e056b8e970218cf1
Restyled.io <commits@restyled.io>
2020-07-12 00:00:30 +0000

    Restyled by reorder-python-imports

--- test_espeakng.py
+++ test_espeakng.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python3
-
 # An eSpeak NG TTS binding for Python3.
 # Copyright (C) 2016-2020 Sayak Brahmachari.
 ##
@@ -14,7 +13,6 @@
 ##
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import time
 
 import espeakng
