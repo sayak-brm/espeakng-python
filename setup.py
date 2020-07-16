34c079f100c8efbae23179f8452c4fd826da1dec
Sayak B <sayak-brm@users.noreply.github.com>
2020-07-16 07:02:57 +0530

    Fix conflicting licences

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
